3c1a5fd05a7979b597c5f0dc87eba422a3cd3b77
acenawaz01 <46965770+acenawaz01@users.noreply.github.com>
2022-05-25 12:58:15 +0530

    Create sum.java

---
+++ Java/sum.java
@@ -0,0 +1,11 @@
+class sum
+
+         public static void main (String args [])
+         {
+                int a,b,c;
+                a=5;
+                b=10;
+                c=a+b;
+                System.out.println("Sum is "+c);
+         }
+}
