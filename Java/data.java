a3fdd8d8948ddaf0da3adf9dac525257ca1adc20
acenawaz01 <46965770+acenawaz01@users.noreply.github.com>
2022-05-25 12:57:37 +0530

    Create data.java

---
+++ Java/data.java
@@ -0,0 +1,12 @@
+class data
+{
+         public static void main (String args [])
+         {
+                int a;
+                a=5;
+                System.out.println(a);
+                System.out.println("a");
+                a=6;
+                System.out.println(a);
+         }
+}
