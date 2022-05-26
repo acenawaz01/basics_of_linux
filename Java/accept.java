2387f8447b105c401124bba6c56f1b73794595fe
acenawaz01 <46965770+acenawaz01@users.noreply.github.com>
2022-05-26 12:01:15 +0530

    Create accept.java

---
+++ Java/accept.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+class accept
+{
+        public static void main(String args[])
+        {
+                Scanner sc= new Scanner(System.in);
+
+                System.out.println("enter first number");
+                int a = sc.nextInt();
+
+                System.out.println("enter second number");
+                int b = sc.nextInt();
+
+                int c = a+b;
+                int d = a*b;
+                int e = a/b;
+                int f = a-b;
+                System.out.println(c);
+                System.out.println(d);
+                System.out.println(e);
+                System.out.println(f);
+
+
+
+
+                System.out.println(c);
+        }
+}
